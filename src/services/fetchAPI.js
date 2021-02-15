529a27e320c5bfba3cc6d638ae32dbe28c1495ed
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-14 22:23:37 -0800

    added services file fetchAPI

---
+++ src/services/fetchAPI.js
@@ -0,0 +1,14 @@
+export default function fetchAPI(url, method, results){
+    if(method !== 'GET'){
+        return fetch(url, {
+            method,
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            results
+        })
+            .then(res = res.json());   
+    }
+    return fetch(url)
+        .then (res => res.json());
+}

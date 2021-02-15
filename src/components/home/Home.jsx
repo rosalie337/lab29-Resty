ec92889b6560a33a37e1612e97fb8be02b4e21f0
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-14 21:16:00 -0800

    added home, header files

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Header from '../header/Header';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <h1>Home Page!</h1>
+            </div>
+        )
+    }
+}
